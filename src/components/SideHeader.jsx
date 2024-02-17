4e85d6eb9c3857196cdb6e327d20cee6895d36d7
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-02-17 16:18:18 +0100

    ajout style bouton sidebar

--- src/components/SideHeader.jsx
+++ src/components/SideHeader.jsx
@@ -8,14 +8,13 @@ const SideHeader = () => {
 
   return (
     <>
-      <div style={{ display: "flex", height: "100%", minHeight: "400px" }}>
+      <div className="prose" style={{ display: "flex", height: "100%", minHeight: "400px" }}>
         <Sidebar
           collapsed={!openSidebar}
           onBackdropClic={() => setOpenSidebar(false)}
           toggled={openSidebar}
           backgroundColor="#bcf3ff"
           breakPoint="all"
-          className={`transition-width duration-500`}
         >
           <Menu>
             <MenuItem> Documentation</MenuItem>
@@ -23,14 +22,16 @@ const SideHeader = () => {
             <MenuItem> E-commerce</MenuItem>
             <MenuItem> Examples</MenuItem>
           </Menu>
-          <div className="flex justify-center mt-10 cursor-pointer">
-          <img
-            className="sb-button"
-            src={cross}
-            alt="close sidebar"
-            width={30}
-            onClick={() => setOpenSidebar(!openSidebar)}
-          />
+          <div className="mt-10 flex cursor-pointer justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              src={cross}
+              alt="close sidebar"
+              width={30}
+              onClick={() => setOpenSidebar(!openSidebar)}
+            >
+              Sortie
+            </button>
           </div>
         </Sidebar>
         <main style={{ display: "flex", padding: 10 }}>
