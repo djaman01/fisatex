61379017a155adface6d99e992f5bf6fbac41dd8
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-02-17 17:03:34 +0100

    react icons et fin sidebar

--- src/components/SideHeader.jsx
+++ src/components/SideHeader.jsx
@@ -2,29 +2,38 @@ import { useState } from "react";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import { useMyContext } from "../ContextComp";
 import { cross } from "../assets/icons";
+import { IoHomeOutline } from "react-icons/io5";
+import { RiProductHuntLine } from "react-icons/ri";
+import { BsInfoSquare } from "react-icons/bs";
+import { MdOutlineContactPhone } from "react-icons/md";
 
 const SideHeader = () => {
   const { openSidebar, setOpenSidebar } = useMyContext();
 
   return (
     <>
-      <div className="prose" style={{ display: "flex", height: "100%", minHeight: "400px" }}>
+      <div
+        className="prose"
+        style={{ display: "flex", height: "100%", minHeight: "400px" }}
+      >
         <Sidebar
-          collapsed={!openSidebar}
           onBackdropClic={() => setOpenSidebar(false)}
           toggled={openSidebar}
-          backgroundColor="#bcf3ff"
+          backgroundColor="#9BB7D4"
           breakPoint="all"
         >
-          <Menu>
-            <MenuItem> Documentation</MenuItem>
-            <MenuItem> Calendar</MenuItem>
-            <MenuItem> E-commerce</MenuItem>
-            <MenuItem> Examples</MenuItem>
+          <Menu transitionDuration={300}>
+            <MenuItem icon={<IoHomeOutline size={18} />}> Accueil </MenuItem>
+            <SubMenu icon={<RiProductHuntLine size={20} />} label="Produits">
+              <MenuItem> Gants</MenuItem>
+              <MenuItem> Tissus Gants</MenuItem>
+            </SubMenu>
+            <MenuItem icon={<BsInfoSquare />}> À propos </MenuItem>
+            <MenuItem icon={<MdOutlineContactPhone size={18} />}> Contact </MenuItem>
           </Menu>
           <div className="mt-10 flex cursor-pointer justify-center">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
               src={cross}
               alt="close sidebar"
               width={30}
