979c3850adb94ddb4c17f197199f0f9510df7f77
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-02-22 23:58:37 +0100

    correction responsivité

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -2,7 +2,7 @@ const MyButton = ({label, iconURL, fullWidth}) => {
   return (
     //${fullWidth && 'w-full'} veut dire que si le components a un props fullWidth activé, alors on lui donne le style w-full
     <button
-      className={`flex items-center justify-center gap-2 h-14 rounded-full border border-coral-red bg-coral-red px-7 py-4 font-montserrat text-lg leading-none text-white ${fullWidth && "w-full"}`}
+      className={`flex items-center justify-center gap-2 h-14 rounded-full border border-coral-red bg-coral-red px-7 py-4 font-montserrat text-xl leading-none text-white ${fullWidth && "w-full"}`}
     >
       {label}
       {/* Condition pour ne montrer une cicone dans le button, que s'il y a une icone dispo */}
--- src/sections/VotreGant.jsx
+++ src/sections/VotreGant.jsx
@@ -4,13 +4,13 @@ import MyButton from "../components/MyButton";
 
 const VotreGant = () => {
   return (
-    <section className=" flex items-center border bg-red-100 py-12 max-lg:flex-col">
-      <div className="mt-[-50px] px-24 ">
+    <section className=" flex items-center border bg-red-100 py-12 max-lg:py-20 max-lg:flex-col max-lg:items-center">
+      <div className="mt-[-50px] px-24 max-lg:pb-14 ">
         <h2 className="font-palanquin text-4xl font-bold ">
           <span className="text-coral-red">Personnalisez</span> Votre Gant !
         </h2>
         {/* max-w-lg:Limite la largeur du texte pour qu'il ne prenne pas trop de place */}
-        <p className="mt-4 font-montserrat text-lg leading-8 lg:max-w-lg sm:max-w-sm">
+        <p className="mt-4 font-montserrat text-2xl leading-10 lg:max-w-lg">
           Choisissez parmi une <b>vaste sélection de couleurs</b> et ajoutez{" "}
           <b>votre logo</b> sur l'élastique, afin de créer un produit{" "}
           <b>unique</b> qui correspond à votre marque
