e7905cfd07cdd119431ed42d1770868c544eb06a
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-02-22 23:07:42 +0100

    fait 3rd part version pc

--- src/sections/VotreGant.jsx
+++ src/sections/VotreGant.jsx
@@ -1,18 +1,22 @@
+import { arrowRight } from "../assets/icons";
 import { gantsFond } from "../assets/images";
 import MyButton from "../components/MyButton";
 
 const VotreGant = () => {
   return (
     <section className=" flex items-center border bg-red-100 py-12 max-lg:flex-col">
-      <div className="px-24 mt-[-50px] ">
+      <div className="mt-[-50px] px-24 ">
         <h2 className="font-palanquin text-4xl font-bold ">
           <span className="text-coral-red">Personnalisez</span> Votre Gant !
         </h2>
-        <p className="mt-4 lg:max-w-lg">Chez Fisatex, nous vous offrons la possibilité de personnaliser votre gant: Choisissez parmis une large gamme de couleurs et votre logo sur l'élastique</p>
-        <p>C'est aussi possible pour le Kit-visage et le frotte-dos</p>
-        {/* info-text et un custom style écrit dans index.js */}
+        {/* max-w-lg:Limite la largeur du texte pour qu'il ne prenne pas trop de place */}
+        <p className="mt-4 font-montserrat text-lg leading-8 lg:max-w-lg sm:max-w-sm">
+          Choisissez parmi une <b>vaste sélection de couleurs</b> et ajoutez{" "}
+          <b>votre logo</b> sur l'élastique, afin de créer un produit{" "}
+          <b>unique</b> qui correspond à votre marque
+        </p>
         <div className="mt-6">
-          <MyButton label="View Details info-text" />
+          <MyButton label="Contactez-nous" iconURL={arrowRight} />
         </div>
       </div>
 
