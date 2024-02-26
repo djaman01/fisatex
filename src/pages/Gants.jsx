84542d2a12ac2e11c8a022ace7e7f5eb4fbf86d9
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-02-26 14:12:18 +0100

    fin page gants style pc

--- src/pages/Gants.jsx
+++ src/pages/Gants.jsx
@@ -10,24 +10,22 @@ const Gants = () => {
       </header>
 
       <main>
-        <div className=" grid grid-cols-[500px,1fr] grid-rows-[500px,500px,500px] items-center gap-8 ml-40">
+        <div className=" mb-20 ml-52 grid grid-cols-[500px,1fr] grid-rows-[500px,500px,500px] items-center gap-8">
           <img
             src={gantAroma}
             alt="gant peaux sensibles"
             className="h-5/6 w-5/6 rounded-l-full"
           />
 
-          <div className="prose h-5/6 w-5/6 rounded-r-full bg-slate-300 shadow-3xl">
-            <h2 className="pt-2 pl-24 text-blue-600">
-              Gant Peaux Sensibles
-            </h2>
-            <ul className="pt-2 font-bold ">
+          <div className="prose h-5/6 w-5/6 rounded-r-full bg-slate-300 pt-20 shadow-3xl">
+            <h2 className="pl-24 pt-2 text-blue-600">Gant Peaux Sensibles</h2>
+            <ol className="pt-2 font-bold ">
               <li>Notre gant de gommage pour les peaux sensibles</li>
               <li>
                 Associe un gommage performant tout en étant respectueux des
                 peaux fragiles et sujettes aux irritations.
               </li>
-            </ul>
+            </ol>
           </div>
 
           <img
@@ -35,14 +33,12 @@ const Gants = () => {
             alt="gant Traditionel"
             className="h-5/6 w-5/6 rounded-l-full"
           />
-          <div className="prose h-5/6 w-5/6 rounded-r-full bg-slate-300 shadow-3xl">
-            <h2 className="pt-2 pl-24 text-blue-600">
-              Gant Traditionnel
-            </h2>
-            <ul className="pt-2 font-bold">
+          <div className="prose h-5/6 w-5/6 rounded-r-full bg-slate-300 pt-20 shadow-3xl">
+            <h2 className="pl-24 pt-2 text-blue-600">Gant Traditionnel</h2>
+            <ol className="pt-2 font-bold">
               <li>Le gant de gommage Classique</li>
               <li>Convient aux peaux résistantes</li>
-            </ul>
+            </ol>
           </div>
 
           <img
@@ -51,14 +47,14 @@ const Gants = () => {
             className="h-5/6 w-5/6 rounded-l-full"
           />
 
-          <div className="prose h-5/6 w-5/6 rounded-r-full bg-slate-300 shadow-3xl">
-            <h2 className="pt-2 pl-24 text-blue-600">
+          <div className="prose h-5/6 w-5/6 rounded-r-full bg-slate-300 pt-20 shadow-3xl">
+            <h2 className="pl-24 pt-2 text-blue-600">
               Gant Qualité Supérieure
             </h2>
-            <ul className="pt-2 font-bold">
+            <ol className="pt-2 font-bold">
               <li>Notre gant de gommage avec la technologie la plus aboutie</li>
               <li>Convient à tout type de peaux et offre un gommage optimal</li>
-            </ul>
+            </ol>
           </div>
         </div>
       </main>
