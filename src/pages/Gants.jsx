106e1c1a38b87b5743c81a2c6f7dae7254c90cc4
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-02-26 13:22:03 +0100

    style avancée page gants

--- src/pages/Gants.jsx
+++ src/pages/Gants.jsx
@@ -8,19 +8,19 @@ const Gants = () => {
       <header>
         <Header />
       </header>
-      <main className="mx-16 mt-5">
-        <div className="grid grid-cols-[500px,1fr] grid-rows-[500px,500px,500px] ">
+      <main className="mx-16 mt-10">
+        <div className="ml-10 grid grid-cols-[500px,1fr] grid-rows-[500px,500px,500px] ">
           <img
             src={gantAroma}
             alt="gant peaux sensibles"
             className="h-5/6 w-5/6 rounded-l-full"
           />
 
-          <div className="m prose h-3/6 border border-black">
+          <div className="prose ml-14 h-3/6 rounded-r-full bg-slate-300 shadow-3xl">
             <h2 className="pt-2 text-center text-blue-600">
               Gant Peaux Sensibles
             </h2>
-            <ul className="pt-2">
+            <ul className="pt-2 font-bold">
               <li>Notre gant de gommage pour les peaux sensibles</li>
               <li>
                 Associe un gommage performant tout en étant respectueux des
@@ -34,11 +34,11 @@ const Gants = () => {
             alt="gant Traditionel"
             className="h-5/6 w-5/6 rounded-l-full"
           />
-          <div className="prose h-3/6 border border-black">
+          <div className="prose ml-14 h-3/6 rounded-r-full bg-slate-300 shadow-3xl">
             <h2 className="pt-2 text-center text-blue-600">
               Gant Traditionnel
             </h2>
-            <ul className="pt-2">
+            <ul className="pt-2 font-bold">
               <li>Le gant de gommage Classique</li>
               <li>Convient aux peaux résistantes</li>
             </ul>
@@ -50,11 +50,11 @@ const Gants = () => {
             className="h-5/6 w-5/6 rounded-l-full"
           />
 
-          <div className="prose h-3/6 border border-black">
+          <div className="prose ml-14 h-3/6 rounded-r-full bg-slate-300 shadow-3xl">
             <h2 className="pt-2 text-center text-blue-600">
               Gant Qualité Supérieure
             </h2>
-            <ul className="pt-2">
+            <ul className="pt-2 font-bold">
               <li>Notre gant de gommage avec la technologie la plus aboutie</li>
               <li>Convient à tout type de peaux et offre un gommage optimal</li>
             </ul>
