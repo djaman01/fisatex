b1490102b1f8960312c4cf122c047b2a244833e0
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-03-05 10:04:35 +0100

    arrangement commentaire formik

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,70 +1,151 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Formik, Form, useField } from "formik";
+import * as Yup from "yup";
 
-const FormContact= () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .max(15, 'Must be 15 characters or less')
-        .required('Required'),
-      lastName: Yup.string()
-        .max(20, 'Must be 20 characters or less')
-        .required('Required'),
-      email: Yup.string().email('Invalid email address').required('Required'),
-    }),
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input>. We can use field meta to show an error
+  // message if the field is invalid and it has been touched (i.e. visited)
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
-      />
-      {formik.touched.firstName && formik.errors.firstName ? (
-        <div>{formik.errors.firstName}</div>
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
       ) : null}
+    </>
+  );
+};
 
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
-      />
-      {formik.touched.lastName && formik.errors.lastName ? (
-        <div>{formik.errors.lastName}</div>
+  // When you specify `type` to useField(), it will
+  // return the correct bag of props for you -- a `checked` prop will be included
+  // in `field` alongside `name`, `value`, `onChange`, and `onBlur`
+const MyCheckbox = ({ children, ...props }) => {
+  const [field, meta] = useField({ ...props, type: "checkbox" });
+  return (
+    <div>
+      <label className="checkbox-input">
+        <input type="checkbox" {...field} {...props} />
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
       ) : null}
+    </div>
+  );
+};
 
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
+const MySelect = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
       ) : null}
+    </div>
+  );
+};
 
-      <button type="submit">Submit</button>
-    </form>
+// And now we can use these templates to create different inpiuts easily
+const FormContact = () => {
+
+  return (
+    <>
+      <div className="mx-auto mb-4 max-w-md rounded bg-white px-8 pb-8 pt-6 shadow-md">
+
+        <Formik
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            acceptedTerms: false, // added for our checkbox
+            jobType: "", // added for our select
+          }}
+          validationSchema={Yup.object({
+            firstName: Yup.string()
+              .max(15, "Must be 15 characters or less")
+              .required("Required"),
+            lastName: Yup.string()
+              .max(20, "Must be 20 characters or less")
+              .required("Required"),
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Required"),
+            acceptedTerms: Yup.boolean()
+              .required("Required")
+              .oneOf([true], "You must accept the terms and conditions."),
+            jobType: Yup.string()
+              .oneOf(
+                ["designer", "development", "product", "other"],
+                "Invalid Job Type",
+              )
+              .required("Required"),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+
+          <Form>
+            <MyTextInput
+              label="First Name"
+              name="firstName"
+              type="text"
+              placeholder="Jane"
+              className=" focus:border-blue-500  block w-full appearance-none rounded border px-3 py-2 leading-tight focus:outline-none"
+            />
+
+            <MyTextInput
+              label="Last Name"
+              name="lastName"
+              type="text"
+              placeholder="Doe"
+              className="focus:ring-blue-500 block w-full appearance-none rounded border px-3 py-2 leading-tight focus:outline-none"
+            />
+
+            <MyTextInput
+              label="Email Address"
+              name="email"
+              type="email"
+              placeholder="jane@formik.com"
+              className="focus:ring-blue-500 block w-full appearance-none rounded border px-3 py-2 leading-tight focus:outline-none"
+            />
+
+            <MySelect
+              label="Job Type"
+              name="jobType"
+              className="focus:ring-blue-500 block w-full appearance-none rounded border border-gray-300 px-3 py-2 leading-tight focus:outline-none"
+            >
+              <option value="">Select a job type</option>
+              <option value="designer">Designer</option>
+              <option value="development">Developer</option>
+              <option value="product">Product Manager</option>
+              <option value="other">Other</option>
+            </MySelect>
+
+            <MyCheckbox name="acceptedTerms">
+              I accept the terms and conditions
+            </MyCheckbox>
+
+            <button
+              type="submit"
+              className="focus:ring-blue-500  rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+            >
+              Submit
+            </button>
+          </Form>
+          
+        </Formik>
+      </div>
+    </>
   );
 };
 
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -4,12 +4,7 @@ import Header from "../components/Header";
 
 import { IoMailOutline } from "react-icons/io5";
 
-
-
-
 const ContactUs = () => {
- 
-
   return (
     <>
       <header>
@@ -63,8 +58,8 @@ const ContactUs = () => {
             </div>
           </div>
 
-          <div className="grid grid-cols-2">
-            <div>
+          <div className="grid grid-cols-2 border border-black padding-b">
+            <div className="border border-black">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.5190186332275!2d-7.612445924970957!3d33.59183544190202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda7cd62408f3ef9%3A0xf0df71d96535da97!2s7%20Rue%20Bouchaib%20El%20Machrouhi%2C%20Casablanca%2020250!5e0!3m2!1sfr!2sma!4v1709150151948!5m2!1sfr!2sma"
                 width="600"
@@ -73,14 +68,17 @@ const ContactUs = () => {
                 allowFullScreen=""
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
+                className="w-full"
               ></iframe>
             </div>
 
+            <div className="border border-black">
+            <FormContact />
           </div>
 
-          <div>
-              <FormContact />
-            </div>
+          </div>
+
+          
         </div>
       </main>
 
