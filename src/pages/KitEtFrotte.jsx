1c37c43b7546e9d6dd9a7474609085bc8ec7b863
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-03-14 13:11:49 +0000

    responsivité page kit et frotte-dos

--- src/pages/KitEtFrotte.jsx
+++ src/pages/KitEtFrotte.jsx
@@ -19,7 +19,8 @@ const KitEtFrotte = () => {
       </header>
 
       <main>
-        <div className="bg-[#f0f0f0] rounded-lg p-5 mx-40 my-10 grid grid-cols-2 gap-y-5">
+        <div className="max-lg: my-10 rounded-lg bg-[#f0f0f0] p-5 max-lg:flex max-lg:flex-col max-lg:gap-y-5 lg:mx-40 lg:grid lg:grid-cols-2 lg:gap-y-5">
+
           <div>
             <img
               src={kit1}
@@ -28,8 +29,7 @@ const KitEtFrotte = () => {
             />
           </div>
 
-          <div className="prose row-span-2 ml-9">
-
+          <div className="prose row-span-2 max-lg:order-last lg:ml-9">
             <h2 className="text-center text-blue-600">Kit Visage</h2>
             <div className="pt-2 font-bold max-lg:pl-5 max-lg:pt-0 lg:pl-5 ">
               <li> Notre gant de gommage spécial Visage</li>
@@ -60,18 +60,21 @@ const KitEtFrotte = () => {
               className="w-[600px] rounded-lg"
             />
           </div>
+
         </div>
 
-        <div className="bg-[#f0f0f0] rounded-lg p-5 mx-40 my-10 grid grid-cols-2 gap-y-5">
+
+        <div className="max-lg: my-10 rounded-lg bg-[#f0f0f0] p-5 max-lg:flex max-lg:flex-col max-lg:gap-y-5 lg:mx-40 lg:grid lg:grid-cols-2 lg:gap-y-5">
+
           <div>
             <img
-              src={frotteDos}
+              src={kit1}
               alt="kit-visage-face"
-              className="h-[400px] w-[600px] rounded-lg"
+              className="w-[600px] rounded-lg"
             />
           </div>
 
-           <div className="prose row-span-2 ml-9">
+          <div className="prose row-span-2 max-lg:order-last lg:ml-9">
             <h2 className="text-center text-blue-600">Frotte-Dos</h2>
             <div className="pt-2 font-bold max-lg:pl-5 max-lg:pt-0 lg:pl-5 ">
               <li> Notre produit pour un gommage facile et efficace du Dos</li>
@@ -79,10 +82,12 @@ const KitEtFrotte = () => {
                 Fait avec le tissus du gant de gommage pour{" "}
                 <span className="text-blue-600">peaux sensible </span>
               </li>
+
               <li>
                 Dispose aussi d'une{" "}
                 <span className="text-blue-600"> face pour le Savonnage</span>
               </li>
+
               <li style={{ color: "#ff0921" }}>
                 Disponible dans une large variété de couleurs
               </li>
@@ -94,13 +99,16 @@ const KitEtFrotte = () => {
 
           <div>
             <img
-              src={frotteDosLogo}
+              src={kit2}
               alt="kit-visage-dos"
-              className="h-[600px] w-[600px] rounded-lg"
+              className="w-[600px] rounded-lg"
             />
           </div>
+
         </div>
+
       </main>
+
       <footer className="padding-x bg-gray-800 pb-8 pt-8">
         <Footer />
       </footer>
@@ -109,5 +117,3 @@ const KitEtFrotte = () => {
 };
 
 export default KitEtFrotte;
-
-
