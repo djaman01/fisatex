63a1b1248701bc974c9e643c2346bab7141c489f
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-03-15 13:44:51 +0000

    ajout bordure text responsive page kit et frotte-dos

--- src/pages/KitEtFrotte.jsx
+++ src/pages/KitEtFrotte.jsx
@@ -28,7 +28,7 @@ const KitEtFrotte = () => {
             />
           </div>
 
-          <div className="prose row-span-2 shadow-2xl max-lg:order-last max-lg:rounded-3xl max-lg:py-5 lg:ml-9 lg:rounded-r-full lg:border lg:border-slate-300 ">
+          <div className="prose row-span-2 shadow-2xl max-lg:order-last max-lg:rounded-3xl max-lg:py-5 lg:ml-9 lg:rounded-r-full lg:border lg:border-slate-300 max-lg:border max-lg:border-slate-400 ">
             <h2 className=" text-blue-600 max-lg:text-center lg:pl-20 lg:pt-5">
               Kit Visage
             </h2>
@@ -72,7 +72,7 @@ const KitEtFrotte = () => {
             />
           </div>
 
-          <div className="prose row-span-2 shadow-2xl max-lg:order-last max-lg:rounded-3xl max-lg:py-5 lg:ml-9 lg:rounded-r-full lg:border lg:border-slate-300 ">
+          <div className="prose row-span-2 shadow-2xl max-lg:order-last max-lg:rounded-3xl max-lg:py-5 lg:ml-9 lg:rounded-r-full lg:border lg:border-slate-300 max-lg:border max-lg:border-slate-400 ">
             <h2 className=" text-blue-600 max-lg:text-center lg:pl-20 lg:pt-5">
               Frotte-Dos
             </h2>
